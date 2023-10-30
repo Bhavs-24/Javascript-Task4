34ac899b7a1b679bd7b0052b9cd8f72021ba97ec
Bhavs-24 <bhavani.meka@healthonus.io>
2023-10-30 22:53:59 +0530

    functionality in js

--- contactform.js
+++ contactform.js
@@ -9,6 +9,20 @@ if(emailpattern.test(emailInput.value)){
     validationMessage.textContent = "Invalid email address";
 }
 
+// document.getElementById("contact_form").addEventListener("submit",function(){
+    // event.preventDefault();
+
+var nameValue = document.getElementById("name").value;
+var emailValue = document.getElementById("email").value;
+var orgvale = document.getElementById("organizationname").value;
+
+if(nameValue.trim() === "" || emailValue.trim ==="" || orgvale.trim ===""){
+    document.getElementById("errorMessage").innerHTML ="Please fill all the required details below";
+}
+else{
+    document.getElementById("errorMessage").innerHTML = "";
+}
+//});
 }
 function radiofunc(radio){
     if(radio.checked){
@@ -19,14 +33,44 @@ function radiofunc(radio){
 function clearForm(){
     document.getElementById('contact_form').reset();
 }
-// function State(){
-    var dropdown = document.getElementById('statedropdowns');
-    var promoinputfield = document.getElementById('promocode');
 
-    dropdown.addEventListener("change",function(){
-        var selectedValue = dropdown.options[dropdown.selectedIndex].value;
-        var newValue = selectedValue + "- PROMO";
-        promoinputfield.value = newValue;
-    });
-// }
-    // $(field).val($(field).val() + dropdown.value);
+var dropdown = document.getElementById('statedropdowns');
+var promoinputfield = document.getElementById('promocode');
+dropdown.addEventListener("change",function(){
+    var selectedValue = dropdown.options[dropdown.selectedIndex].value;
+    var newValue = selectedValue + "- PROMO";
+    promoinputfield.value = newValue;
+});
+
+function namechange(){
+    var nameValue = document.getElementById("name").value;
+    if(nameValue.length==0){
+        document.getElementById('nameerror').innerHTML = "Please enter name"
+    }
+} 
+function emailchange(){
+    var emailValue = document.getElementById("email").value;
+    if(emailValue.length==0){
+        document.getElementById('emailerror').innerHTML = "Please enter email"
+    }
+}
+function orgchange(){
+    var orgvale = document.getElementById("organizationname").value;
+    if(orgvale.length==0){
+        document.getElementById('orgerror').innerHTML = "Please enter organization name"
+    }
+}
+function namefocus(){
+    var nameValue = document.getElementById("name").value;
+    var emailValue = document.getElementById("email").value;
+    var orgvale = document.getElementById("organizationname").value;
+    if(nameValue.length!=0){
+        document.getElementById('nameerror').innerHTML = ""
+    }
+    if(emailValue.length!=0){
+        document.getElementById('emailerror').innerHTML = ""
+    }
+    if(orgvale.length!=0){
+        document.getElementById('orgerror').innerHTML = "";
+    }
+}
