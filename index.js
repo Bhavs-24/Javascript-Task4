cb108f7df60b60b306897e24606854f3e3279be0
Bhavs-24 <bhavani.meka@healthonus.io>
2023-10-30 11:58:57 +0530

    careers form

---
+++ index.js
@@ -0,0 +1,20 @@
+{/* <script type="text/javascript"> */}
+    window.onload = function () {
+        var fileupload = document.getElementById('file_upload');
+        var filepath = document.getElementById('file_path');
+        var button = document.getElementById('btn_fileupload');
+
+        button.onclick = function (){
+            fileupload.click();
+        };
+        fileupload.onchange = function(){
+            var filename = fileupload.value.split('\\')[fileupload.value.split('\\').length - 1];
+            filepath.innerHTML = "<b>Uploaded Resume </b>" + filename;
+        };
+    };
+    function onSubmit(){
+        alert("Successfully submitted!");
+        return true;
+    }
+
+// </script>
